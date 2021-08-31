e936097a5fd5884ff3ebbc3d83eae99492e6e870
muath-khatib <mo3az.khatib@hotmail.com>
2021-08-31 16:43:02 +0300

    resolve problem

--- Src/Controllers/index.js
+++ Src/Controllers/index.js
@@ -1,7 +1,11 @@
 const signUpUser = require('./controller.signup');
 const signInUser = require('./conrtoller.signin');
+const newPost = require('./controller.new-post');
+const setCookies = require('./controller.set-cookies');
 
 module.exports = {
   signUpUser,
   signInUser,
+  newPost,
+  setCookies,
 };
