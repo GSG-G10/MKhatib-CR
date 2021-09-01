f7e3c5d848850087f642895411cc8029cd3224cb
muath-khatib <mo3az.khatib@hotmail.com>
2021-09-01 08:17:14 +0300

    Resolving 2

--- Src/Middlewares/midlleware.get-trending-news.js
+++ Src/Middlewares/midlleware.get-trending-news.js
@@ -2,15 +2,19 @@ require('dotenv').config();
 
 const axios = require('axios');
 
-const API_KEY = {
+const options = {
+  method: 'GET',
+  url: 'https://bing-news-search1.p.rapidapi.com/news/trendingtopics',
+  params: { safeSearch: 'Off', textFormat: 'Raw' },
   headers: {
     'x-bingapis-sdk': 'true',
     'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
-    'x-rapidapi-key': process.env.TRENDING_NEWS_API_KEY,
+    'x-rapidapi-key': process.env.API_KEY,
   },
 };
 module.exports = (req, res) => {
-  axios.request('https://bing-news-search1.p.rapidapi.com/news/trendingtopics', API_KEY)
+  axios.request('https://bing-news-search1.p.rapidapi.com/news/trendingtopics', options)
     .then((response) => res.json(response.data.value.splice(0, 4)))
+    .then(() => console.log(options))
     .catch((error) => res.json(error));
 };
