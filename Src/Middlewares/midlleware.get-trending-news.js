276b90b675811c34eb7024822a5b19dc899649fd
muath-khatib <mo3az.khatib@hotmail.com>
2021-09-01 04:23:44 +0300

    resolve_1 for deply

--- Src/Middlewares/midlleware.get-trending-news.js
+++ Src/Middlewares/midlleware.get-trending-news.js
@@ -4,6 +4,8 @@ const axios = require('axios');
 
 const API_KEY = {
   headers: {
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
     'x-rapidapi-key': process.env.TRENDING_NEWS_API_KEY,
   },
 };
